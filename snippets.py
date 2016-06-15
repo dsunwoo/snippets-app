b3033ad584492677cd99127905ea45170bbe6389
Daniel Sunwoo <dsunwoo@yahoo.com>
2016-06-15 20:02:08 +0000

    Updated code to use put and get for PostgreSQL DB

--- snippets.py
+++ snippets.py
@@ -1,7 +1,14 @@
+import psycopg2
 import logging
 import argparse
 # Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
+
+# Connect to database
+logging.debug("Connecting to PostgreSQL")
+connection=psycopg2.connect(database="snippets")
+logging.debug("Database connection established")
+
 
 def put(name, snippet):
     """
@@ -9,8 +16,12 @@ def put(name, snippet):
     
     Returns the name and the snippet
     """
-    logging.error("FIXME: Unimplemented - put({!r}, {!r})"
-    .format(name, snippet))
+    logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
+    cursor=connection.cursor()
+    command="insert into snippets values (%s, %s)"
+    cursor.execute(command, (name, snippet))
+    connection.commit()
+    logging.debug("Snippet stored successfully.")
     return name, snippet
     
 def get(name):
@@ -20,14 +31,44 @@ def get(name):
     
     Returns the snippet.
     """
-    logging.error("FIXME: Unimplemented -get({!r})".format(name))
-    return ""
+    logging.info("Retrieving snippet {!r}".format(name))
+    cursor=connection.cursor()
+    command="select message from snippets where keyword=(%s)"
+    cursor.execute(command, (name,))
+    row=cursor.fetchone()
+    logging.debug("Message for Snippet Keyword {0} retrieved successfully"
+    .format(name))
+    return row
 
 def main():
     """Main function"""
     logging.info("Constructing parser")
     parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
-    arguments = parser.parse_args()
+    
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
+    # Subparser for the put command
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    put_parser.add_argument("name", help="Name of the snippet")
+    put_parser.add_argument("snippet", help="Snippet text")
+    
+    # Subparser for the get command
+    logging.debug("Constructing get subparser")
+    get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
+    get_parser.add_argument("name", help="Keyword in snippets DB")
+    
+    arguments = parser.parse_args()
+    # Convert parsed arguments from Namespace to dictionary
+    arguments = vars(arguments)
+    command = arguments.pop("command")
+
+    if command == "put":
+        name, snippet = put(**arguments)
+        print("Stored {!r} as {!r}".format(snippet, name))
+    elif command == "get":
+        snippet = get(**arguments)
+        print("Retrieved snippet: {!r}".format(snippet))
+        
 if __name__ == "__main__":
     main()
